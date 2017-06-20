80b1e85c4aca3c094fbb6349c973f64db5450136
Scott <marden.scott@gmail.com>
2017-06-20 11:48:06 -0700

    Finished functionality for assignment. Not styled at all. Pulls items from list in random order based on number given by user.

--- apps/surprise_app/views.py
+++ apps/surprise_app/views.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+import random
+
+values = ['Holy Toledo!', 'WHAT IS HAPPENING IN OAKLAND, RAY FOSSE?!', 'I HAVE NO IDEA, GLEN KUIPER!', 'They are walkin off again!', 'Other things!!', 'Supercalifragilisticexpialidocious', 'HotDog!', 'Appearing Ninja!', 'Disappearing Ninja', ]
 
 # Create your views here.
+def index(request):
+	return render(request, 'surprise_app/index.html')
+
+def submit(request):
+	surprises = []
+	if request.method == "POST":
+		loop = 0
+		for i in range(int(request.POST['count'])):
+			randnum = random.randint(0, len(values)-1)
+			surprises += [values[randnum]]
+		request.session['list'] = surprises
+
+	return redirect('/results')
+
+def results(request):
+	context = {
+		'list': request.session['list']
+	}
+	return render(request, 'surprise_app/results.html', context)
